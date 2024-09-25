8271554ba327a086723a64cf451fb23af72fcd82
Rico Ferrante <disenowebmdp@gmail.com>
2024-09-25 15:40:42 -0400

    settings vue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/cumberlandmuseum/'  // Replace with your GitHub repository name
+    : '/',
   transpileDependencies: true
 })
